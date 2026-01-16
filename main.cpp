d4142722d1d88a695d05eb95f91f4b62ae56c314
Shreetej Hadge <shreetejhadge1308@gmail.com>
2026-01-15 23:08:03 -0700

    feat(navbar): Implement TUI navigation bar

--- main.cpp
+++ main.cpp
@@ -2,12 +2,40 @@
 #include <ftxui/component/screen_interactive.hpp> // Import the engine that manages the terminal window
 #include <ftxui/dom/elements.hpp>                 // Import visual elements (text, border, vbox, filler)
 
+using namespace ftxui; // Save typing: allows writing 'text' instead of 'ftxui::text'
+
+Element RenderNavbar()
+{
+    FlexboxConfig config;
+    config.justify_content = FlexboxConfig::JustifyContent::SpaceEvenly;
+
+    return flexbox({
+        text(" shreetej hadge ") | center,
+        separatorLight() | color(Color::GrayDark),
+        hbox({text("a"), text(" about me") | color(Color::GrayDark) | center}),
+        separatorLight() | color(Color::GrayDark),
+        hbox({text("e"), text(" experience") | color(Color::GrayDark) | center}),
+        separatorLight() | color(Color::GrayDark),
+        hbox({text("p"), text(" projects") | color(Color::GrayDark) | center}),
+        separatorLight() | color(Color::GrayDark),
+        hbox({text("c"), text(" contact me") | color(Color::GrayDark) | center})
+    }, config) | borderStyled(LIGHT, Color::GrayDark);
+}
+
+// Element blinker() {
+    
+// }
+
+
 int main()
 {
-    using namespace ftxui; // Save typing: allows writing 'text' instead of 'ftxui::text'
 
     auto renderer = Renderer([] { // Create a component that defines how to draw the UI
-        return filler() | border; // "filler()" takes all space, "| border" draws a box around it
+        Element navbar = RenderNavbar();
+        
+        auto body = filler() | border;
+
+        return vbox({navbar,body});
     });
 
     auto screen = ScreenInteractive::Fullscreen(); // Initialize the screen in "Alternate Buffer" mode (like vim)
